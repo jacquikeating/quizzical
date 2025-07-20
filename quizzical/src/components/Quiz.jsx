ca638948217ad2bedf977a08434581cfdf9ab55a
jacquikeating <j.keating@live.ca>
2025-07-20 14:59:45 -0400

    Built out Quiz component
    - Created a <form> element within the Quiz component.
    - Created two .map() functions to render each item in the quiz state as an <article>, with the question rendered as a <legend> and the array of all_answers rendered as radio buttons.
    - Wrote a handleSubmit() function. When the user clicks the <button>, handleSubmit() maps over the original quiz array from props, adding extra properties for the user's selected answer and whether it was correct. It returns the new array as completedQuiz, which it then console.logs() and passes to setQuiz().

--- quizzical/src/components/Quiz.jsx
+++ quizzical/src/components/Quiz.jsx
@@ -1,5 +1,38 @@
-export default function Quiz() {
+export default function Quiz({ quiz, setQuiz}) {
+
+    function handleSubmit(formData) {
+
+        let completedQuiz = quiz.map((q, index) => {
+            let selectedAnswer = formData.get(`q${index}`)
+            let isCorrect = selectedAnswer == q.correct_answer
+            let completedQuestion = {...q, selected_answer: selectedAnswer, isCorrect: isCorrect}
+            return (completedQuestion)
+        })
+
+        console.log(completedQuiz)
+        setQuiz(completedQuiz)
+    }
+    
     return (
-        <>I'm the quiz!</>
+        <form action={handleSubmit}>
+            {quiz.map((q, index) => {
+                let questionNum = `q${index}`
+
+                return (
+                    <article className="question" key={questionNum}>
+                        <legend>{q.question}</legend>
+                        {q.all_answers.map((a, index) => {
+                            return (
+                                <label key={index}>
+                                    <input type="radio" name={questionNum} value={a} />
+                                    {a}
+                                </label>
+                            )
+                        })}
+                    </article>
+                )
+            })}
+            <button>Check answers</button>
+        </form>
     )
 }
