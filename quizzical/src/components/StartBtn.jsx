edf6faf63b9efadfe710b115c007efd0fefd4bc3
jacquikeating <j.keating@live.ca>
2025-07-20 17:02:22 -0400

    Built StartBtn for play-again context
    - Added an else if block to return button text when context is play-again.
    - The buttons now have an ID instead of a className.
    - Styled button#play-again.

--- quizzical/src/components/StartBtn.jsx
+++ quizzical/src/components/StartBtn.jsx
@@ -4,9 +4,11 @@ export default function StartBtn({ context, startNewQuiz }) {
 
     if (context === "start") {
         text = "Start quiz"
+    } else if (context === "play-again") {
+        text = "Play again"
     }
 
     return (
-        <button className={context} onClick={startNewQuiz}>{text}</button>
+        <button id={context} onClick={startNewQuiz}>{text}</button>
     )
 }
