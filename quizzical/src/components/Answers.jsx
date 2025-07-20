6b22e12222b800c8c1f4692d22f476aa358e47ef
jacquikeating <j.keating@live.ca>
2025-07-20 17:30:42 -0400

    Conditionally styled answers
    - Renamed the answers in the Answers component to revealed-answer to fix a bug with the answers in the Quiz component.
    - Imported clsx package.
    - Used clsx to generate appropriate classNames for each answer.

--- quizzical/src/components/Answers.jsx
+++ quizzical/src/components/Answers.jsx
@@ -1,3 +1,4 @@
+import { clsx } from "clsx"
 import StartBtn from "./StartBtn"
 
 export default function Answers({ completedQuiz, setQuizStarted, startNewQuiz }) {
@@ -15,9 +16,13 @@ export default function Answers({ completedQuiz, setQuizStarted, startNewQuiz })
                             <div className="answer-options">
                                 {q.all_answers.map((a, index) => {
                                 const answerNum = questionNum + "a" + index
+                                const clsxClasses = clsx("revealed-answer", {
+                                    correct: a === q.correct_answer,
+                                    incorrect: a === q.selected_answer && a !== q.correct_answer
+                                })
 
                                 return (
-                                    <div key={answerNum} className="answer">
+                                    <div key={answerNum} className={clsxClasses}>
                                         <p>{a}</p>
                                     </div> 
                                 )
