84372342eb3a4f42a59e5f1ab9464b8bc6c5c642
jacquikeating <j.keating@live.ca>
2025-07-20 15:39:25 -0400

    Restructured radio inputs
    - Switched from <input> nested inside <label> to separate elements, wrapped in a <div>.
    - Added a <div> to each question to contain all its answer options in a flex container.

--- quizzical/src/components/Quiz.jsx
+++ quizzical/src/components/Quiz.jsx
@@ -15,20 +15,27 @@ export default function Quiz({ quiz, setQuiz}) {
     
     return (
         <form action={handleSubmit}>
+
             {quiz.map((q, index) => {
                 let questionNum = `q${index}`
 
                 return (
                     <article className="question" key={questionNum}>
                         <legend>{q.question}</legend>
-                        {q.all_answers.map((a, index) => {
+
+                        <div className="answer-options">
+                            {q.all_answers.map((a, index) => {
+                            const answerNum = questionNum + "a" + index
+
                             return (
-                                <label key={index}>
-                                    <input type="radio" name={questionNum} value={a} />
-                                    {a}
-                                </label>
+                                <div key={answerNum} className="answer">
+                                    <input type="radio" name={questionNum} value={a} id={answerNum} className="radio-input" />
+                                    <label htmlFor={answerNum}>{a}</label>
+                                </div> 
                             )
                         })}
+                        </div>
+                        
                     </article>
                 )
             })}
