5f28ace6370a0758bea6d96c467e74bd720a88b4
jacquikeating <j.keating@live.ca>
2025-07-20 13:01:19 -0400

    Filled SplashPage content

--- quizzical/src/components/Splash.jsx
+++ quizzical/src/components/Splash.jsx
@@ -3,6 +3,8 @@ import StartBtn from "./StartBtn"
 export default function Splash() {
     return (
         <section>
+            <h1>Quizzical</h1>
+            <p>Test your knowledge!</p>
             <StartBtn context="splash" />
         </section>
     )
