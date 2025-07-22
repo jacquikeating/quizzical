75c5dd1cd13acb8f23c2417e08578ff7989ab4cc
jacquikeating <j.keating@live.ca>
2025-07-21 21:42:02 -0400

    Added fallback options
    - Created a fallback <div> that is rendered in the Quiz component when getQuizFromAPI() fails.
    - I encountered frequent 429 errors during development. While refreshing reliably fixed the issue, any users encountering that empty white page would probably be very confused.
    - Added a second Button that calls getQuizFromAPI() for user convenience.

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -1,59 +1,74 @@
-import { useState, useEffect } from 'react'
-import { decode } from 'html-entities'
-import Splash from './components/Splash'
-import Quiz from './components/Quiz'
-import Answers from './components/Answers'
+import { useState, useEffect } from "react";
+import { decode } from "html-entities";
+import Splash from "./components/Splash";
+import Quiz from "./components/Quiz";
+import Answers from "./components/Answers";
 
 function App() {
-  const [quizStarted, setQuizStarted] = useState(false)
-  const [quiz, setQuiz] = useState(null)
-  const [completedQuiz, setCompletedQuiz] = useState(null)
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [quiz, setQuiz] = useState();
+  const [completedQuiz, setCompletedQuiz] = useState(null);
 
   async function getQuizFromAPI() {
-    const res = await fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple")
-    const data = await res.json()
-    const rawQuestions = data.results
+    const res = await fetch(
+      "https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple"
+    );
+    const data = await res.json();
+    const rawQuestions = data.results;
     const questions = rawQuestions.map((q) => {
-      const answerOptions = [...q.incorrect_answers]
-      const randomIndex = Math.floor(Math.random() * 4)
-      answerOptions.splice(randomIndex, 0, q.correct_answer)
-      const decodedAnswers = decode(answerOptions.join("|")).split("|")
-      const decodedQuestion = decode(q.question)
-      return ({...q, question: decodedQuestion, all_answers: decodedAnswers, correct_answer: decode(q.correct_answer) })
-    })
-    setQuiz(questions)
+      const answerOptions = [...q.incorrect_answers];
+      const randomIndex = Math.floor(Math.random() * 4);
+      answerOptions.splice(randomIndex, 0, q.correct_answer);
+      const decodedAnswers = decode(answerOptions.join("|")).split("|");
+      const decodedQuestion = decode(q.question);
+      return {
+        ...q,
+        question: decodedQuestion,
+        all_answers: decodedAnswers,
+        correct_answer: decode(q.correct_answer),
+      };
+    });
+    setQuiz(questions);
   }
 
   useEffect(() => {
-    getQuizFromAPI()
-  }, [])
+    getQuizFromAPI();
+  }, []);
 
   function startNewQuiz() {
-    setQuizStarted(true)
+    setQuizStarted(true);
 
     if (completedQuiz) {
-        setCompletedQuiz(null)
-        getQuizFromAPI()
+      setCompletedQuiz(null);
+      getQuizFromAPI();
     }
   }
 
   function renderContent() {
     if (completedQuiz) {
-      return <Answers completedQuiz={completedQuiz} startNewQuiz={startNewQuiz} />
+      return (
+        <Answers completedQuiz={completedQuiz} startNewQuiz={startNewQuiz} />
+      );
     } else if (quizStarted && !completedQuiz) {
-      return <Quiz quiz={quiz} setCompletedQuiz={setCompletedQuiz} />
+      return (
+        <Quiz
+          quiz={quiz}
+          setCompletedQuiz={setCompletedQuiz}
+          getQuizFromAPI={getQuizFromAPI}
+        />
+      );
     } else if (!quizStarted) {
-      return <Splash startNewQuiz={startNewQuiz} />    
+      return <Splash startNewQuiz={startNewQuiz} />;
     }
   }
-  
+
   return (
     <main>
       <img src="src/assets/blob-left.png" id="blob-left" aria-hidden="true" />
       <img src="src/assets/blob-right.png" id="blob-right" aria-hidden="true" />
       {renderContent()}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- quizzical/src/components/Quiz.jsx
+++ quizzical/src/components/Quiz.jsx
@@ -1,46 +1,64 @@
-import Button from "./Button"
+import Button from "./Button";
 
-export default function Quiz({ quiz, setCompletedQuiz}) {
+export default function Quiz({ quiz, setCompletedQuiz, getQuizFromAPI }) {
+  function handleSubmit(formData) {
+    let completedQuiz = quiz.map((q, index) => {
+      let selectedAnswer = formData.get(`q${index}`);
+      let isCorrect = selectedAnswer == q.correct_answer;
+      let completedQuestion = {
+        ...q,
+        selected_answer: selectedAnswer,
+        isCorrect: isCorrect,
+      };
+      return completedQuestion;
+    });
 
-    function handleSubmit(formData) {
+    setCompletedQuiz(completedQuiz);
+  }
 
-        let completedQuiz = quiz.map((q, index) => {
-            let selectedAnswer = formData.get(`q${index}`)
-            let isCorrect = selectedAnswer == q.correct_answer
-            let completedQuestion = {...q, selected_answer: selectedAnswer, isCorrect: isCorrect}
-            return (completedQuestion)
-        })
-
-        setCompletedQuiz(completedQuiz)
-    }
-    
-    return (
+  return (
+    <>
+      {quiz ? (
         <form action={handleSubmit}>
+          {quiz.map((q, index) => {
+            let questionNum = `q${index}`;
 
-            {quiz.map((q, index) => {
-                let questionNum = `q${index}`
+            return (
+              <article className="question" key={questionNum}>
+                <legend>{q.question}</legend>
 
-                return (
-                    <article className="question" key={questionNum}>
-                        <legend>{q.question}</legend>
+                <div className="answer-options">
+                  {q.all_answers.map((a, index) => {
+                    const answerNum = questionNum + "a" + index;
 
-                        <div className="answer-options">
-                            {q.all_answers.map((a, index) => {
-                            const answerNum = questionNum + "a" + index
-
-                            return (
-                                <div key={answerNum} className="answer">
-                                    <input type="radio" name={questionNum} value={a} id={answerNum} className="radio-input" />
-                                    <label htmlFor={answerNum}>{a}</label>
-                                </div> 
-                            )
-                        })}
-                        </div>
-                        
-                    </article>
-                )
-            })}
-            <Button size="medium">Check answers</Button>
+                    return (
+                      <div key={answerNum} className="answer">
+                        <input
+                          type="radio"
+                          name={questionNum}
+                          value={a}
+                          id={answerNum}
+                          className="radio-input"
+                        />
+                        <label htmlFor={answerNum}>{a}</label>
+                      </div>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+          <Button size="medium">Check answers</Button>
         </form>
-    )
+      ) : (
+        <div className="quiz-fallback">
+          <h2>Something went wrong!</h2>
+          <p>Give it a sec and try again.</p>
+          <Button size="medium" onClick={getQuizFromAPI}>
+            Try again
+          </Button>
+        </div>
+      )}
+    </>
+  );
 }
